d2a6ae20394f165fa4139363808d483f3b0c26d0
Jacob Lichner <jacob.d.lichner@gmail.com>
2011-10-21 11:35:18 -0700

    Added styles helper

--- lib/site.rb
+++ lib/site.rb
@@ -1,9 +1,17 @@
 
+def select_items regex
+  @items.select { |i| i.identifier =~ regex }
+end
+
 def chapters
-  @items.select { |i| i.identifier =~ %r{^/chapters/} }
+  select_items %r{^/chapters/}
 end
 
 def titleize(identifier)
   filename = identifier.split("/").last
   filename.split("-")[1..-1].map { |w| w.capitalize }.join(" ")
+end
+
+def styles
+  select_items(%r{/styles/styles/}).first
 end
