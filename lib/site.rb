07d51b89a4bbb0c19c65282422cbf816e4c9da29
Jacob Lichner <jacob.d.lichner@gmail.com>
2011-11-02 15:02:03 -0700

    Added explicit Array to fix cross-os ordering

--- lib/site.rb
+++ lib/site.rb
@@ -4,9 +4,9 @@ CAS_VERSION = "3.4.1"
 ### View helpers
 
 def chapters
-  @items
+  Array.new(@items
     .select  { |i| i.identifier =~ %r{^/chapters/} }
-    .sort_by { |i| i.identifier }
+   ).sort_by { |i| i.identifier }
 end
 
 def first_chapter
